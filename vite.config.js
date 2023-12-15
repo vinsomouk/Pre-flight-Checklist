101229485f971da8dc563978a536f4ee7ebb76e3
vinsomouk <diakitekhalidou1@gmail.com>
2023-12-15 15:38:41 +0100

    initial-commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
+import {
+    defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react'
+import {
+    resolve
+} from "path";
 
-// https://vitejs.dev/config/
+const aliases = {
+    '@crema': 'src/@crema',
+    'core': 'src/core',
+    'assets': 'src/assets',
+    '@hook': 'src/@hook',
+    'components': 'src/components',
+    'features': 'src/features',
+    'guards': 'src/guards',
+    'pages': 'src/pages',
+    'types': 'src/types',
+};
+
+const resolvedAliases = Object.fromEntries(
+    Object.entries(aliases).map(([key, value]) => [key, resolve(__dirname, value)]),
+);
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            external: [
+                "react", // ignore react stuff
+                "react-dom",
+            ],
+        }
+    },
+    resolve: {
+        alias: {
+            ...resolvedAliases,
+            './runtimeConfig': './runtimeConfig.browser',
+            'jss-plugin-{}': 'jss-plugin-global'
+        },
+    },
+});
